1178368bd4b62b9975e50d71fc9292833c9c99b0
GieniuG <bartoszlwowski@gmail.com>
2026-06-23 00:42:48 +0200

    Fix date not being correct

--- src/js/content.js
+++ src/js/content.js
@@ -64,8 +64,8 @@ function main(){
         const h=String(d.getHours()).padStart(2,"0")
         const m=String(d.getMinutes()).padStart(2,"0")
         const s=String(d.getSeconds()).padStart(2,"0")
-        const D=String(d.getDay()).padStart(2,"0")
-        const M=String(d.getMonth()).padStart(2,"0")
+        const D=String(d.getDate()).padStart(2,"0")
+        const M=String(d.getMonth()+1).padStart(2,"0")
         const Y=String(d.getFullYear()).padStart(2,"0")
         time.innerHTML = `${h}:${m}:${s}`
         date.innerHTML = `${D}.${M}.${Y}`
